5c9c5ee69e73a5b39f56e6a878252f99f768245b
Tomas Bayer <mail@tomas.berlin>
2022-08-22 20:50:29 +0200

    Add database caching

--- django_model_validation/models.py
+++ django_model_validation/models.py
@@ -1,16 +1,33 @@
-from typing import Iterator, Optional
+from typing import Iterator, Optional, Type
 
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Manager, Q, QuerySet
 from django.db.models.base import ModelBase
 
 from django_model_validation.utils import collect_validation_errors
 from django_model_validation.validators import ModelValidator
 
 
-def validator(*, auto: bool = True):
+def validator(
+        *,
+        auto: bool = True,
+        cache: bool = False,
+        auto_use_cache: bool = True,
+        auto_update_cache: bool = True,
+        cache_field_name: Optional[str] = None,
+        cache_field_verbose_name: Optional[str] = None,
+):
     def decorator(method):
-        return ModelValidator(method, auto)
+        return ModelValidator(
+            method,
+            auto,
+            cache,
+            auto_use_cache,
+            auto_update_cache,
+            cache_field_name,
+            cache_field_verbose_name,
+        )
 
     return decorator
 
@@ -29,11 +46,34 @@ class ModelBaseWithValidators(ModelBase):
         return new_class
 
 
+class ValidatingModelManager(Manager):
+
+    def __init__(self, *, exclude_valid: bool = False, exclude_invalid: bool = False):
+        super().__init__()
+        self.exclude_valid = exclude_valid
+        self.exclude_invalid = exclude_invalid
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.exclude_valid:
+            qs = qs.exclude(self.model.get_custom_validity_condition())
+        if self.exclude_invalid:
+            qs = qs.exclude(~self.model.get_custom_validity_condition())
+        return qs
+
+    def __set_name__(self, model_type: Type['ValidatingModel']):
+        self.model = model_type
+
+
 class ValidatingModel(models.Model, metaclass=ModelBaseWithValidators):
+    objects = ValidatingModelManager()
+    valid_objects = ValidatingModelManager(exclude_valid=True)
+    invalid_objects = ValidatingModelManager(exclude_invalid=True)
 
     @classmethod
     def _register_validator(cls, model_validator: ModelValidator) -> None:
         cls._model_validators.append(model_validator)
+        model_validator._register_for_model()
 
     def get_custom_validator_errors(self, *, use_all: bool = False) -> Iterator[ValidationError]:
         for model_validator in self._model_validators:
@@ -48,22 +88,39 @@ class ValidatingModel(models.Model, metaclass=ModelBaseWithValidators):
         if errors:
             raise ValidationError(errors)
 
-    def check_custom_validators(self, *, use_all: bool = False) -> bool:
+    def check_custom_validators(
+            self,
+            *,
+            use_all: bool = False,
+            use_caches: Optional[bool] = None,
+    ) -> bool:
         for model_validator in self._model_validators:
             if use_all is True or model_validator.auto:
-                if not model_validator.get_instance_validator(self).is_valid():
+                if not model_validator.get_instance_validator(self).is_valid(use_cache=use_caches):
                     return False
 
         return True
 
-    def get_custom_validator_results(self, *, use_all: bool = False) -> dict[str, bool]:
+    def get_custom_validator_results(
+            self,
+            *,
+            use_all: bool = False,
+            use_caches: Optional[bool] = None,
+    ) -> dict[str, bool]:
         return {
-            model_validator.name: model_validator.get_instance_validator(self).is_valid()
+            model_validator.get_cache_field_name(): model_validator.get_instance_validator(self).is_valid(
+                use_cache=use_caches)
             for model_validator in self._model_validators
             if use_all is True or model_validator.auto
         }
 
-    def is_valid(self, *args, use_custom_validators: Optional[bool] = None, **kwargs) -> bool:
+    def is_valid(
+            self,
+            *args,
+            use_custom_validators: Optional[bool] = None,
+            use_validator_caches: Optional[bool] = None,
+            **kwargs,
+    ) -> bool:
         try:
             super().full_clean(*args, **kwargs)
         except ValidationError:
@@ -71,6 +128,7 @@ class ValidatingModel(models.Model, metaclass=ModelBaseWithValidators):
 
         return use_custom_validators is False or self.check_custom_validators(
             use_all=use_custom_validators is True,
+            use_caches=use_validator_caches,
         )
 
     @collect_validation_errors
@@ -82,6 +140,82 @@ class ValidatingModel(models.Model, metaclass=ModelBaseWithValidators):
 
         if use_custom_validators is not False:
             yield from self.get_custom_validator_errors(use_all=use_custom_validators is True)
+
+    def update_validator_caches(self, *, update_all: bool = False):
+        for model_validator in self._model_validators:
+            if model_validator.cache and (update_all is True or model_validator.auto_update_cache):
+                model_validator.get_instance_validator(self).update_cache()
+
+    def clear_validator_caches(self, *, clear_all: bool = False):
+        for model_validator in self._model_validators:
+            if model_validator.cache and (clear_all is True or model_validator.auto_update_cache):
+                model_validator.get_instance_validator(self).clear_cache()
+
+    def are_validation_results_cached(self):
+        return all(
+            model_validator.get_instance_validator(self).is_cached()
+            for model_validator in self._model_validators
+            if model_validator.cache
+        )
+
+    def save(self, *args, update_validator_caches: Optional[bool] = None, **kwargs):
+        if update_validator_caches is not False:
+            self.update_validator_caches(update_all=update_validator_caches is True)
+
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def update_validator_caches_globally(cls, queryset: Optional[QuerySet] = None, *, update_all: bool = False):
+        for model_validator in cls._model_validators:
+            if model_validator.cache and (update_all is True or model_validator.auto_update_cache):
+                model_validator.update_cache(queryset)
+
+    @classmethod
+    def clear_validator_caches_globally(cls, queryset: Optional[QuerySet] = None, *, clear_all: bool = False):
+        if queryset is None:
+            queryset = cls.objects.all()
+
+        fields = [
+            model_validator.get_cache_field_name()
+            for model_validator in cls._model_validators
+            if model_validator.cache and (clear_all is True or model_validator.auto_update_cache)
+        ]
+
+        queryset.update(**{field: None for field in fields})
+
+    @classmethod
+    def get_are_validation_results_cached_condition(cls) -> Q:
+        condition = ~Q(pk__in=[])
+
+        for model_validator in cls._model_validators:
+            if model_validator.cache:
+                condition &= model_validator.get_is_cached_condition()
+
+        return condition
+
+    @classmethod
+    def are_validation_results_cached_globally(cls, queryset: Optional[QuerySet] = None) -> bool:
+        if queryset is None:
+            queryset = cls.objects.all()
+
+        return queryset.filter(cls.get_are_validation_results_cached_condition()).exists()
+
+    @classmethod
+    def get_custom_validity_condition(cls) -> Q:
+        condition = ~Q(pk__in=[])
+
+        for model_validator in cls._model_validators:
+            if model_validator.cache:
+                condition &= model_validator.get_is_valid_condition()
+
+        return condition
+
+    @classmethod
+    def check_custom_validators_globally(cls, queryset: Optional[QuerySet] = None) -> bool:
+        if queryset is None:
+            queryset = cls.objects.all()
+
+        return queryset.filter(cls.get_custom_validity_condition()).exists()
 
     class Meta:
         abstract = True
--- django_model_validation/validators.py
+++ django_model_validation/validators.py
@@ -3,9 +3,16 @@ from types import GeneratorType
 from typing import TYPE_CHECKING, Callable, Optional, Type
 
 from django.core.exceptions import ValidationError
+from django.db.models import Q, QuerySet
+
+from django_model_validation.cache_field import ModelValidatorCacheField
 
 if TYPE_CHECKING:
     from django_model_validation.models import ValidatingModel
+
+
+class ValidatorHasNoCacheError(Exception):
+    pass
 
 
 @dataclass
@@ -13,13 +20,15 @@ class ModelInstanceValidator:
     model_validator: 'ModelValidator'
     model_instance: 'ValidatingModel'
 
-    def get_validation_error(self) -> Optional[ValidationError]:
+    def _get_validation_error(self) -> Optional[ValidationError]:
         try:
             result = self.model_validator.method(self.model_instance)
 
             if isinstance(result, bool):
                 if not result:
-                    return ValidationError(f"The validator \"{self.model_validator.name}\" failed.")
+                    return ValidationError(
+                        f"The validator \"{self.model_validator.get_cache_field_verbose_name()}\" failed.",
+                    )
                 else:
                     return None
 
@@ -33,27 +42,75 @@ class ModelInstanceValidator:
 
         return None
 
-    def validate(self) -> None:
-        validation_error = self.get_validation_error()
+    def get_validation_error(self, *, update_cache: bool = True) -> Optional[ValidationError]:
+        validation_error = self._get_validation_error()
+
+        if self.model_validator.cache and update_cache:
+            setattr(
+                self.model_instance,
+                self.model_validator.get_cache_field_name(),
+                validation_error is None,
+            )
+
+        return validation_error
+
+    def validate(self, *, update_cache: bool = True) -> None:
+        validation_error = self.get_validation_error(update_cache=update_cache)
 
         if validation_error is not None:
             raise validation_error
 
-    def is_valid(self) -> bool:
+    def is_valid(self, *, use_cache: Optional[bool] = None, update_cache: bool = True) -> bool:
+        if self.model_validator.cache:
+            if use_cache is None:
+                use_cache = self.model_validator.auto_use_cache
+
+            if use_cache:
+                cache_value = self.get_cache()
+                if cache_value is not None:
+                    return cache_value
+
         try:
-            self.validate()
+            self.validate(update_cache=update_cache)
             return True
         except ValidationError:
             return False
 
-    def __call__(self) -> None:
-        self.validate()
+    def is_cached(self) -> bool:
+        return self.get_cache() is not None
+
+    def get_cache(self) -> Optional[bool]:
+        try:
+            return getattr(self.model_instance, self.model_validator.get_cache_field_name())
+        except AttributeError as err:
+            raise ValidatorHasNoCacheError() from err
+
+    def update_cache(self) -> None:
+        try:
+            return setattr(self.model_instance, self.model_validator.get_cache_field_name(),
+                           self.is_valid(use_cache=False))
+        except AttributeError as err:
+            raise ValidatorHasNoCacheError() from err
+
+    def clear_cache(self) -> None:
+        try:
+            return setattr(self.model_instance, self.model_validator.get_cache_field_name(), None)
+        except AttributeError as err:
+            raise ValidatorHasNoCacheError() from err
+
+    def __call__(self, *args, **kwargs) -> None:
+        self.validate(*args, **kwargs)
 
 
 @dataclass
 class ModelValidator:
     method: Callable
     auto: bool
+    cache: bool
+    auto_use_cache: bool
+    auto_update_cache: bool
+    cache_field_name: Optional[str]
+    cache_field_verbose_name: Optional[str]
 
     model_type: Type['ValidatingModel'] = field(init=False, default=None)
 
@@ -63,6 +120,95 @@ class ModelValidator:
 
     def get_instance_validator(self, obj: 'ValidatingModel') -> ModelInstanceValidator:
         return ModelInstanceValidator(self, obj)
+
+    def get_cache_field_name(self) -> str:
+        if self.cache_field_name is None:
+            return f'is_{self.name}_successful'
+        else:
+            return self.cache_field_name
+
+    def get_cache_field_verbose_name(self) -> str:
+        if self.cache_field_verbose_name is None:
+            return self.get_cache_field_name()
+        else:
+            return self.cache_field_verbose_name
+
+    def _register_for_model(self) -> None:
+        if self.cache:
+            self.model_type.add_to_class(
+                self.get_cache_field_name(),
+                ModelValidatorCacheField(verbose_name=self.cache_field_verbose_name),
+            )
+
+    def get_is_valid_condition(self) -> Q:
+        if self.cache:
+            return Q(**{self.get_cache_field_name(): True})
+        else:
+            raise ValidatorHasNoCacheError()
+
+    def get_is_invalid_condition(self, *, include_unknown_validity: bool = False) -> Q:
+        if self.cache:
+            if include_unknown_validity:
+                return ~Q(**{self.get_cache_field_name(): True})
+            else:
+                return Q(**{self.get_cache_field_name(): False})
+        else:
+            raise ValidatorHasNoCacheError()
+
+    def get_is_cached_condition(self) -> Q:
+        if self.cache:
+            return Q(**{f'{self.get_cache_field_name()}__isnull': False})
+        else:
+            raise ValidatorHasNoCacheError()
+
+    def get_valid_objects(self, queryset: Optional[QuerySet] = None) -> QuerySet:
+        if queryset is None:
+            queryset = self.model_type.objects
+
+        return queryset.filter(self.get_is_valid_condition())
+
+    def get_invalid_objects(
+            self,
+            queryset: Optional[QuerySet] = None,
+            *,
+            include_unknown_validity: bool = False,
+    ) -> QuerySet:
+        if queryset is None:
+            queryset = self.model_type.objects
+
+        return queryset.filter(self.get_is_invalid_condition(include_unknown_validity=include_unknown_validity))
+
+    def is_all_valid(self, queryset: Optional[QuerySet] = None) -> bool:
+        if queryset is None:
+            queryset = self.model_type.objects.all()
+
+        return self.get_invalid_objects(queryset).exists()
+
+    def is_all_cached(self, queryset: Optional[QuerySet] = None) -> bool:
+        if queryset is None:
+            queryset = self.model_type.objects.all()
+
+        return not queryset.filter(~self.get_is_cached_condition()).exists()
+
+    def update_cache(self, queryset: Optional[QuerySet] = None) -> None:
+        if not self.cache:
+            raise ValidatorHasNoCacheError()
+
+        if queryset is None:
+            queryset = self.model_type.objects.all()
+
+        for obj in queryset:
+            self.get_instance_validator(obj).update_cache()
+            obj.save(update_validator_caches=False)
+
+    def clear_cache(self, queryset: Optional[QuerySet] = None) -> None:
+        if not self.cache:
+            raise ValidatorHasNoCacheError()
+
+        if queryset is None:
+            queryset = self.model_type.objects.all()
+
+        queryset.update(**{self.get_cache_field_name(): None})
 
     def __set_name__(self, model_type: Type['ValidatingModel'], name: str):
         self.model_type = model_type
